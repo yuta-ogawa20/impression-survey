aeaa210872268136f3a74bee599436b84c4b371f
yuta-ogawa20 <62197658+yuta-ogawa20@users.noreply.github.com>
2022-01-27 20:58:04 +0900

    Update random_url.js

--- random_url.js
+++ random_url.js
@@ -2,9 +2,9 @@
 <!–
 myCnt = 3;//urlの数を入力
 myTable = new Array(
-“https://www.soscisurvey.de/test296199/?act=keIf0SOO2A5zDRBNrZXTv5PD”,//以下任意のurlを入力
-“https://www.soscisurvey.de/test296199/?act=avZh1PvNLDzNL1kYsYSHHmwj”,
-“https://www.soscisurvey.de/test296199/?act=KHQlReFGYJplMhhjE9cRZBQL″//最後はコンマ無し
+"https://www.soscisurvey.de/test296199/?act=keIf0SOO2A5zDRBNrZXTv5PD",//以下任意のurlを入力
+"https://www.soscisurvey.de/test296199/?act=avZh1PvNLDzNL1kYsYSHHmwj",
+"https://www.soscisurvey.de/test296199/?act=KHQlReFGYJplMhhjE9cRZBQL"//最後はコンマ無し
 );
 
 function myRndLink(){
