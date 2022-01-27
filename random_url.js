375455148b554b0a658727a0913f02d0c68b2117
yuta-ogawa20 <62197658+yuta-ogawa20@users.noreply.github.com>
2022-01-27 20:29:06 +0900

    Create random_url.js

---
+++ random_url.js
@@ -0,0 +1,15 @@
+<script type=”text/javascript”>
+<!–
+myCnt = ;//urlの数を入力
+myTable = new Array(
+“url1”,//以下任意のurlを入力
+“url2”,
+“url3″//最後はコンマ無し
+);
+
+function myRndLink(){
+myRnd = Math.floor(Math.random()*myCnt);
+location.href = myTable[myRnd];
+}
+–>
+</script>
